a855199e23733d449b22d3a96d765a944ea54dcf
Jörgen Brandt <joergen.brandt@onlinehome.de>
2017-04-18 14:08:35 +0200

    insert_coin function fixed. remove_cookie_box function added in CVM example. Boiler plate code added.

--- src/cvm.erl
+++ src/cvm.erl
@@ -1,3 +1,25 @@
+%% -*- erlang -*-
+%%
+%% An example collection for the gen_pnet behavior.
+%%
+%% Copyright 2017 Jorgen Brandt
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%    http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+%% @author Jorgen Brandt <joergen.brandt@onlinehome.de>
+%% @version 0.1.0
+%% @copyright 2017 Jorgen Brandt
+
 -module( cvm ).
 
 -export( [code_change/3, handle_call/3, handle_cast/2, handle_info/2,
@@ -18,7 +40,10 @@ start_link() ->
   gen_pnet:start_link( ?MODULE, [] ).
 
 insert_coin( Pid ) ->
-  gen_pnet:cast( Pid, insert_coin ).
+  gen_pnet:call( Pid, insert_coin ).
+
+remove_cookie_box( Pid ) ->
+  gen_pnet:call( Pid, remove_cookie_box ).
 
 %%====================================================================
 %% Interface callback functions
